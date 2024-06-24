207cea604a6b77e1a7f984059493e5cc0fb811c5
Syed zain ali shah <137421662+zain-23@users.noreply.github.com>
2024-06-24 20:56:04 +0500

    complete the coding challenge

--- app.js
+++ app.js
@@ -14,5 +14,24 @@ Person.prototype.calcAge = function () {
   return this.birthYear + 10;
 };
 
-console.log(khizer.calcAge());
-console.log(jonas.calcAge());
+// Coding challenge with constructor function
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+const bmw = new Car("BMW", 120);
+const mercedes = new Car("Mercedes", 150);
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`${this.make} is going at ${this.speed}km/h`);
+};
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`${this.make} is going at ${this.speed}km/h`);
+};
+
+console.log(bmw.accelerate());
+console.log(bmw.brake());
